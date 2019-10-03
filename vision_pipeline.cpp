8452b2841dab9d28bd08a66ad38a87b908735ff0
tomfoo <3643395+tomfoo@users.noreply.github.com>
2019-10-03 00:26:55 +0000

    did something

--- vision_pipeline.cpp
+++ vision_pipeline.cpp
@@ -22,6 +22,33 @@ int main(int args, char** argss){
         std::cerr << "ERROR! Unable to open camera\n";
         return -1;
     }
+    frame.set(CAP_PROP_FRAME_WIDTH,960);
+    frame.set(CAP_PROP_FRAME_HEIGHT,544);
+/*
+
+
+# Full resolution for the MS LifeCam HD-3000 is 1280x720
+RESOLUTION="-r 960x544"
+FPS="-f 20"
+YUYV="-y"
+QUALITY="-q 10" # 0 .. 10
+CONTRAST="-co 5" #0 .. 10
+SHARPNESS="-sh 50" # 0 .. 50
+COLORBALANCE="-cb 90"
+WHITEBALANCE="-wb 2800" # 2800 .. 10000
+# Possible exposure values for the Microsoft LifeCam HD-3000/5000:
+# 5, 9-10 (same exposure?), 19-20 (same exposure?) 39, 78, 156, 312, 625, 1250,
+# 2500, 5000, 10000
+# (per http://comments.gmane.org/gmane.linux.drivers.uvc.devel/5717)
+EXPOSURE="-ex 9"
+BACKLIGHT="-bk 0"
+CHROMAGAIN="-cagc 2800" # 2800 .. 10000
+
+
+*/
+
+
+
     //--- GRAB AND WRITE LOOP
     std::cout << "Start grabbing" << std::endl
         << "Press any key to terminate" << std::endl;
@@ -41,10 +68,12 @@ int main(int args, char** argss){
         for (int r = 0;r<frame.rows;r++){
           std::cout << "[";
           for (int c = 0;c<frame.cols;c++){
-            uint8_t green = frame.data[r*frame.cols*frame.channels() + c*frame.channels() + 0];
+            uint8_t green = frame.data[r*frame.cols*frame.channels() + c*frame.channels() + 1];
             printf("%d,",green);
+            break;
           }
           std::cout << "]";
+          break;
         }
         std::cout << std::endl;
 //        if (cv::waitKey(5) >= 0)
