118b4d82888fde2e6be2fb7620e742e889f0e8fd
carson <carson42g@gmail.com>
2019-11-16 20:02:16 -0500

    general fixes, finished solvePnP code?
    - finished solvePnP code (hopefully)
    - fixed up whole codebase
    - removed commented out lines that will never be uncommented
    - added a couple TODO statements
    - added good debug statements

--- vision_pipeline.cpp
+++ vision_pipeline.cpp
@@ -12,26 +12,38 @@
     if(!STATEMENT){ \
       std::cerr << #STATEMENT << " is not supported by the video backend" << std::endl; \
     };  
-
+/*
+ *
+ * Things to improve:
+ *
+ * - use defalt size values for std::vector when the size is going to be the same every time
+ *
+ *
+ */
 void findContours(cv::Mat &, bool , std::vector<std::vector<cv::Point> > &);
 void filterContours(std::vector<std::vector<cv::Point> > &, double , double , double , double , double , double , double [], double , double , double , double , std::vector<std::vector<cv::Point> > &);
 
-
-//std::tuple<double,double,double>
-cv::Mat findPos(cv::Mat rvec,cv::Mat tvec,int x1, int y1){
-    double xCameraF[] = {static_cast<double>(x1),static_cast<double>(y1),0.0};
-//    printf("tvec type: %d\n",tvec.type());
-    cv::Mat XCamera(3, 1, 6, xCameraF); // CV_64F 
-    cv::Mat rotMat;
-    cv::Rodrigues(rvec, rotMat);
-    cv::Mat tempWorld = XCamera - tvec;
-//    printf("succ. sub. Type: %d\n", tempWorld.type());
-    cv::Mat XWorld = rotMat.t() * (XCamera - tvec);
-//    printf("XWorld type: %d\n",XWorld.type());
-//    printf("XWorld:");
-    //std::cout << XWorld << std::endl;
-//    cv XCamera - tvec 
-    return XWorld;
+// TODO: test
+// returns: x, z, distance, angle1, angle2
+std::vector<double> findPos(cv::Mat rvec,cv::Mat tvec){
+    double x = tvec.at<double>(0,0); 
+    double z = tvec.at<double>(2,0); // there's a chance this should be 0,2
+    double distance = sqrt(pow(x,2) + pow(z,2));
+    double angle1 = atan2(x,z);
+    cv::Mat rotMatrix;
+    cv::Rodrigues(rvec,rotMatrix);
+    std::cout << rotMatrix.type() << std::endl; // TODO make sure this mat is used with the right types
+    cv::Mat worldMat = rotMatrix.inv() * (-tvec);
+   
+    double angle2 = atan2(worldMat.at<double>(0,0), worldMat.at<double>(2,0));// again with the 0,2 possibility
+    
+    std::vector<double> retur(5);
+    retur.push_back(x);
+    retur.push_back(z);
+    retur.push_back(distance);
+    retur.push_back(angle1);
+    retur.push_back(angle2);
+    return retur;
 }
 
 // calling this with points.size < 1 will make me sad
@@ -85,8 +97,9 @@ std::vector<cv::Point> contoursToPoints(std::vector<std::vector<cv::Point>> poin
 #define PI 3.14159265
 
 cv::Point3f flip(cv::Point3f point){
-    return cv::Point3f(-1 * point.x,point.y,point.z);
+       return cv::Point3f(-1 * point.x,point.y,point.z);
 }
+
 std::vector<cv::Point3f> generateWorldConstant(){
     std::cout << "generating world constants...";
     double cosine = cos(TARGET_ROTATION * PI / 180.0);
@@ -114,7 +127,7 @@ std::vector<cv::Point3f> generateWorldConstant(){
 
 
     
-    std::vector<cv::Point3f> fullTarget;
+    std::vector<cv::Point3f> fullTarget(6);
     fullTarget.push_back(flip(right2)); // point 1
     fullTarget.push_back(right2); //       point 2
     fullTarget.push_back(right3); //       point 3
@@ -142,79 +155,27 @@ int main(int args, char** argss){
        "=========" << std::endl;
     std::vector<cv::Point3f> worldTarget = generateWorldConstant();
 
-    std::cout << "premature return" << std::endl;
-    return 0;
 
-    std::cout << "readnig from param.yaml" << std::endl;
+    std::cout << "reading from param.yaml" << std::endl;
 
     cv::FileStorage fs;
     fs.open("param.yaml",cv::FileStorage::READ);
     cv::Mat cameraMatrix = fs["camera_matrix"].mat();// 6: CV_64F
     
-	std::vector<cv::Point3f> objectPoints;
-    fs["grid_points_real"] >> objectPoints;
-    // std::cout << objectPoints;
-    std::vector<cv::Point2f> imagePoints;
-    fs["image_points_real"] >> imagePoints;
-  
-    //cv::Mat imagePoints = fs["image_points"].mat(); // 13: CV_32F
+    
     cv::Mat distCoeff = fs["distortion_coefficients"].mat(); // 6: CV_64F  
    
-    //cv::Mat testImagePoints(imagePoints);
-    //printf("test type:%d\n",testImagePoints.type());
-    //imagePoints = testImagePoints; 
-     std::cout << "obj " << objectPoints << std::endl << "img " << imagePoints << std::endl; 
-    cv::Mat opoints = ((cv::InputArray)objectPoints).getMat();
-    printf("opoints:%d\n",opoints.type());// 21: CV_32FC3
-    cv::Mat ipoints = ((cv::InputArray)imagePoints).getMat();
-    int npoints = std::max(opoints.checkVector(3,/*CV_32F*/5), opoints.checkVector(3, /*CV_64F*/6));
-    bool a = (npoints >= 4);
-    bool b = false;
-    bool c = npoints == std::max(ipoints.checkVector(2,/*CV_32F*/5), ipoints.checkVector(2,/*CV_64F*/6));
-
-    std::cout << "a:" << a << " b:" << b <<  " c:" << c << std::endl;
-    
-    printf("opoints.checkVector(3,5): %d\n",opoints.checkVector(3,5));
-    printf("opoints.checkVector(3,6): %d\n",opoints.checkVector(3,6));
-    printf("npoints:%d\n",npoints);
-    printf("ipoints.checkVector(2,5): %d\n",ipoints.checkVector(2,5));
-    printf("ipoints.checkVector(2,6): %d\n",ipoints.checkVector(2,6)); 
-    printf("opoints -- rows:%d col:%d\n",opoints.rows,opoints.cols);
-    printf("ipoints -- rows:%d col:%d\n",ipoints.rows,ipoints.cols);
-    printf("cameraMatrix: %d iPoints: %d distCoeff: %d opoints: %d\n",
-            cameraMatrix.type(), ipoints.type(), distCoeff.type(),opoints.type());
-
-    //cv::Mat rvec2;
-    //cv::Mat tvec2;
-    //bool no = cv::solvePnP(objectPoints,imagePoints,cameraMatrix,distCoeff,rvec2,tvec2);
-
-    //std::cout << "no: " << no << std::endl;
-    //std::cout << "rvec2: " << rvec2 << std::endl;
-    //std::cout << "tvec2: " << tvec2 << std::endl;
-   
-    //findPos(rvec2,tvec2,0,0);
-    //printf("distance: %f\nangle1:%f\nangle2:%f\n",
-    //        std::get<0>(out),std::get<1>(out),std::get<2>(out)); 
-    // return 0;
-     // YAY!
-    
-    //std::cout << imagePoints;
-    //std::cout << distCoeff;
 
 
-
-    cv::Mat frame;
-    //--- INITIALIZE VIDEOCAPTURE
+    cv::Mat frame; // the next avalible frame is put here
     cv::VideoCapture cap;
-    // open the default camera using default API
-    // cap.open(0);
-    // OR advance usage: select any API backend
+    
     int deviceID = 0;             // 0 = open default camera
     int apiID = cv::CAP_V4L2;      // 0 = autodetect default API
 
 
     cap.open(deviceID + apiID);
-    std::cout << "api: " << apiID << std::endl;
+    std::cout << "api in use: " << apiID << std::endl;
     // set up the camera
     std::cout << "setting up camera" << std::endl;
     SET_WITH_CHECK(cap.set(cv::CAP_PROP_FRAME_WIDTH,288));
@@ -224,14 +185,9 @@ int main(int args, char** argss){
     SET_WITH_CHECK(cap.set(cv::VIDEOWRITER_PROP_QUALITY,10));
     SET_WITH_CHECK(cap.set(cv::CAP_PROP_CONTRAST,5));
     SET_WITH_CHECK(cap.set(cv::CAP_PROP_SHARPNESS,50));
-    // missing - color balance
-    // not found? commenting out bc we don't have cb either // cap.set(cv::CAP_PROP_WB_TEMPERATURE,2800);    
     SET_WITH_CHECK(cap.set(cv::CAP_PROP_AUTO_EXPOSURE,1)); 
     SET_WITH_CHECK(cap.set(cv::CAP_PROP_EXPOSURE,9));
-    // missing - chromagain
-    std::cout << "setting up done" << std::endl;
-//    return 1;
-    // open selected camera using selected API
+    std::cout << "camera setup done" << std::endl;
     
     // check if we succeeded
     if (!cap.isOpened()) {
@@ -240,13 +196,10 @@ int main(int args, char** argss){
     }
 
   
-
-
-    //--- GRAB AND WRITE LOOP
-    for (;;)
-    {
-   //     printf("reading frame...\n");  
+    // global loop, never terminates
+    for (;;){
       // wait for a new frame from camera and store it into 'frame'
+      // this is a blocking call
         cap.read(frame);
         // check if we succeeded
         if (frame.empty()) {
@@ -255,25 +208,14 @@ int main(int args, char** argss){
             continue;
         }
  //       printf("channels: %d\ntype: %d\n",frame.channels(),frame.type());
- //       std::cout << std::endl;
-//        continue;
-        //break;
-        // show live and wait for a key with timeout long enough to show images
-//        imshow("Live", frame);
+        ;
         // std::cout << "cols:" << frame.cols << " rows: " << frame.rows << std::endl;
-        // std::cout << "is empty : " << frame.empty() << "" << std::endl;
-         //uint16_t green = frame.at<uint16_t>(0,0);
-         //printf("%d",green);
-         // printf("Matrix: %d %dx%d \n", frame.type(), frame.cols, frame.rows );
          cv::Mat img_hsv;
          cv::cvtColor(frame,img_hsv,cv::COLOR_RGB2HSV); 
-         frame.release();
- //        cv::Vec3b c = img_hsv.at<cv::Vec3b>(100,100);
- //        printf("%d:%d:%d",c[0],c[1],c[2]);
-         // continue;         
-         // mask by hsvThreashold
-         double hue[] = {33.99280575539568, 93.99317406143345};
-	     double sat[] = {100.89928057553958, 255.0};
+         frame.release(); // release it from memory, saving memory
+
+         double hue[] = {33.99280575539568, 93.99317406143345}; // these are the hue values it must fall between
+	     double sat[] = {100.89928057553958, 255.0}; // sateration is just "amount". Means we need a lot of green
          double val[] = {169.69424460431654, 255.0};
          cv::Mat img_filtered;
          cv::inRange(img_hsv,cv::Scalar(hue[0],sat[0],val[0]),cv::Scalar(hue[1],sat[1],val[1]),img_filtered);
@@ -281,22 +223,22 @@ int main(int args, char** argss){
          // ASSERT img_filtered.type() == 0
          // this is a 8-bit integer with 1 channel
          // https://stackoverflow.com/questions/10167534/how-to-find-out-what-type-of-a-mat-object-is-with-mattype-in-opencv
-         std::vector<std::vector<cv::Point>> og_contours;
+         std::vector<std::vector<cv::Point>> og_contours;// original contours
          findContours(img_filtered,false,og_contours);
          img_filtered.release();
          // filter contours
          std::vector<std::vector<cv::Point>> contours;
-         double filterContoursMinArea = 0;  // default Double
-         double filterContoursMinPerimeter = 100.0;  // default Double
-         double filterContoursMinWidth = 0;  // default Double
-         double filterContoursMaxWidth = 1000;  // default Double
-         double filterContoursMinHeight = 0;  // default Double
-         double filterContoursMaxHeight = 1000;  // default Double
+         double filterContoursMinArea = 0;  
+         double filterContoursMinPerimeter = 100.0;  
+         double filterContoursMinWidth = 0;  
+         double filterContoursMaxWidth = 1000;  
+         double filterContoursMinHeight = 0;  
+         double filterContoursMaxHeight = 1000;  
          double filterContoursSolidity[] = {0, 100};
-         double filterContoursMaxVertices = 1000000;  // default Double
-         double filterContoursMinVertices = 0;  // default Double
-         double filterContoursMinRatio = 0;  // default Double
-         double filterContoursMaxRatio = 1000;  // default Double
+         double filterContoursMaxVertices = 1000000;  
+         double filterContoursMinVertices = 0;  
+         double filterContoursMinRatio = 0;  
+         double filterContoursMaxRatio = 1000;
          filterContours(og_contours, 
                         filterContoursMinArea,
                         filterContoursMinPerimeter,
@@ -309,15 +251,8 @@ int main(int args, char** argss){
                         filterContoursMinVertices,
                         filterContoursMinRatio,
                         filterContoursMaxRatio, contours);
-         //for( uint8_t i = 0; i < contours.size(); i++){
-             //cv::RotatedRect rectangle = cv::minAreaRect(contours[i]); // for testing
-             //rectangle.size;
-             //printf("%fx%frect turned %f degrees at %fx%f",
-             //       rectangle.size.width,rectangle.size.height,
-             //       rectangle.angle,
-             //       rectangle.center.x, rectangle.center.y);
-         //}
-         // first one
+         
+         // TODO make sure it still works when it can see more then one contour
          if( contours.size() == 0){
            printf(" No contours\n");
            continue;
@@ -328,14 +263,11 @@ int main(int args, char** argss){
          }
 
          if( contours.size() != 2){
-//          std::tuple<int,int> xy = findContours       
-            printf("more then one contour found\n");
+             printf("more then one contour found\n");
             continue;
          }
          
          std::vector<cv::Point> points = contoursToPoints(contours);
-//         int x = std::get<0>(xy);
-//         int y = std::get<1>(xy);
          std::cout << "(" <<
              points[0]<<","<<points[1]<<","<<points[2]<<","<<points[3] << "  " << 
              points[4]<<","<<points[5]<<","<<points[6]<<","<<points[7];          
@@ -344,28 +276,24 @@ int main(int args, char** argss){
          std::cout << std::endl;
          continue;
          // m a t h
-         //cv::Mat rvec;
-         //cv::Mat tvec;
-         //cv::solvePnP(objectPoints,imagePoints,cameraMatrix,distCoeff,rvec,tvec);
-         //std::cout << "yes: " << yes << std::endl;
-         //std::cout << "tvec: " << tvec << std::endl;
-         //std::cout << "rvec: " << rvec << std::endl;
-         //cv::RotatedRect rect = cv::minAreaRect(contours[0]);
-         //int x = rect.center.x;
-         //int y = rect.center.y;  
-//         printf("(%10d,%10d)",x,y);
-//         std::cout << std::endl;
-//         continue;                            
-        //cv::Mat xWorld = findPos(rvec, tvec, x, y);
- //       std::cout << "[" << xWorld.at<double>(0) << 
- //                    "," << xWorld.at<double>(1) << 
- //                    "," << xWorld.at<double>(2) << "]";
-       //printf("[%10f,%10f,%10f]",xWorld.at<double>(0),
-       //                          xWorld.at<double>(1),
-       //                          xWorld.at<double>(2)); 
-       //xWorld.release();
-       
-        std::cout << std::endl;
+         cv::Mat rvec;
+         cv::Mat tvec;
+         bool solvePnPSucc = cv::solvePnP(worldTarget,points,cameraMatrix,distCoeff,rvec,tvec);
+         std::cout << "solvePnPSucc: " << solvePnPSucc << std::endl;
+         std::cout << "tvec: " << tvec << std::endl;
+         std::cout << "rvec: " << rvec << std::endl;
+         // TODO make sure that the assumptions made about the types of these mats is correct 
+         std::cout << "tvec type: " << tvec.type() << std::endl;
+         std::cout << "rvec type: " << rvec.type() << std::endl;
+         std::vector<double> pos = findPos(rvec, tvec);
+         double x        = pos[0];
+         double z        = pos[1];
+         double distance = pos[2];
+         double angle1   = pos[3]; 
+         double angle2   = pos[4];
+
+         printf("x:%10f z:%10f distance: %10f angle1: %10f angle2: %10f",x,z,distance,angle1,angle2);
+         std::cout << std::endl;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
